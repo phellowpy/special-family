c02bc53c82db511a8670a64861e5e1ea1234ecfa
Guilherme G Likoski <159035705+GuilhermeGLikoski@users.noreply.github.com>
2025-06-04 09:34:57 -0300

    Create contato.py
    atividade feita pelo João J.

---
+++ back-end/python/contato.py
@@ -0,0 +1,18 @@
+def coletar_informacoes_contato():
+    print("CONTATE-NOS")
+    print("Special Family")
+
+    nome = input("Nome: Digite seu nome:")
+
+    email = input("E-mail: Digite um e-mail válido:")
+
+    mensagem = input("O que podemos ajudar? Mensagem:")
+
+    print ("Informações Enviadasas ")
+    print(f"Nome:")
+    print(f"E-mail:")
+    print(f"Mensagem:")
+    print("Sua mensagem foi recebida com sucesso!")
+
+if __name__ == "__main__":
+        coletar_informacoes_contato()
